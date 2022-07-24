c3cb700ed7a644d7a62a669652b8aca319daf069
Reggie Lee <reg@Reggies-Air.attlocal.net>
2022-07-24 13:35:15 -0500

    Created Book functionality
    -Created the basic script to take user input and create a new Book
    object based on that input. Still working on getting that object and
    displaying an overlay to the body container.

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
-let myLibrary = [];
+let booksLibrary = [];
 
-function Book(title, author, pages, status) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.status = status
+const btn = document.querySelector(".btn");
+const elementul = document.getElementById("book-list");
+
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
 }
 
 function addBookToLibrary() {
-    // do stuff here
+  let elementTitle = document.createElement("p");
+  let elementAuthor = document.createElement("p");
+  let elementPages = document.createElement("p");
+
+  let bookElement = document.createElement("li");
+
+  let titleContent = document.createTextNode("Title: " + book.title);
+  let authorContent = document.createTextNode("Author: " + book.author);
+  let pageContent = document.createTextNode("Pages: " + book.pages);
+
+  elementTitle.appendChild(titleContent);
+  elementAuthor.appendChild(authorContent);
+  elementPages.appendChild(pageContent);
+
+  bookElement.appendChild(elementTitle);
+  bookElement.appendChild(elementAuthor);
+  bookElement.appendChild(elementPages);
+
+  elementUl.appendChild(bookElement);
 }
 
+function submitForm() {
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
 
+  let book = new Book(title, author, pages);
+
+  booksLibrary.push(book);
+
+  addBookToLibrary(book);
+}
+
+btn.addEventListener("click", submitForm);
+console.log(booksLibrary);
